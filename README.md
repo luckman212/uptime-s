2abf485d88e4d5542fe7c43697c37a73e90fc15e
luckman212 <1992842+luckman212@users.noreply.github.com>
2023-06-08 10:59:56 -0400

    initialize

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# uptime-s
+# uptime_s
+
+small console utility that prints the system uptime in seconds
+
+see https://stackoverflow.com/questions/3269321/osx-programmatically-get-uptime
--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+echo -n "compiling..."
 clang -arch arm64 -o uptime_arm.o uptime_s.c
 clang -arch x86_64 -o uptime_x64.o uptime_s.c
 lipo -create -output uptime_s uptime_{arm,x64}.o
-rm uptime_*.o
+[[ -e uptime_s ]] || exit 1
 chmod +x uptime_s
-cp -f uptime_s "${SYNC:-$HOME/Sync}/Binaries/"
-ln -sf "${SYNC:-$HOME/Sync}/Binaries/uptime_s" /usr/local/bin
+echo "done"
+echo -n "testing: "
+./uptime_s
